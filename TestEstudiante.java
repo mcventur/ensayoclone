45cc70eee90e456334089f12399ad562a7aaafba
AntonioDam1 <antonioaguidam1@gmail.com>
2022-10-07 12:16:02 +0200

    Añadi dos estudiantes nuevos

--- TestEstudiante.java
+++ TestEstudiante.java
@@ -18,6 +18,10 @@ public class TestEstudiante
         Estudiante e3 = new Estudiante("Elisa", 6);
         e3.printEstudiante();
         Estudiante e4 = new Estudiante("Antonio", 7);
-	e4.printEstudiante();
+        e4.printEstudiante();
+        Estudiante e5 = new Estudiante("Marce", 5);
+        e5.printEstudiante();
+        Estudiante e6 = new Estudiante("Aitor", 4);
+        e6.printEstudiante();
     }
 }
