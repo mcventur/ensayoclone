167a0a7dfcd41025554a356e05d5d224c42ddaf7
dvilladangossdam1B <divilladangoss1B@gmail.com>
2022-10-10 10:18:07 +0200

    Añadido estudiante 5

--- TestEstudiante.java
+++ TestEstudiante.java
@@ -15,8 +15,11 @@ public class TestEstudiante
         e1.printEstudiante();
         Estudiante e2 = new Estudiante("Ana", 8);
         e2.printEstudiante();
-		Estudiante e3 = new Estudiante("Elisa", 26);
-		e3.printEstudiante();
-
+        Estudiante e3 = new Estudiante("Elisa", 26);
+        e3.printEstudiante();
+        Estudiante e4 = new Estudiante("Dani", 19);
+        e4.printEstudiante();
+        Estudiante e5 = new Estudiante("Alberto", 19);
+        e5.printEstudiante();
     }
 }
